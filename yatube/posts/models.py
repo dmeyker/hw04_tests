6f30a6a087ed64b4e7b6e92b6dea4c8ddeaa0808
Дарья Мейкер <imeyker@gmail.com>
2022-08-28 19:20:27 +0300

    v1.4

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -56,9 +56,8 @@ class Post(models.Model):
         'Картинка',
         upload_to='posts/',
         blank=True
-    )  
+    )
 
-    
     class Meta:
         ordering = ("-pub_date",)
         verbose_name = "Пост"
